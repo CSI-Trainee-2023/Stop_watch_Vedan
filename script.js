546193f5ebd8e05e45c4eca562a8500ac8d65ab4
Vedan8 <vedanjyadav@gmail.com>
2023-10-05 22:09:22 +0530

    functional buttons

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ document.getElementById("start-timer").addEventListener("click", () => {
 
 function displayTimer() {
   milliseconds += 10;
-  seconds = milliseconds == 1000? (seconds + 1) % 60 : seconds;
-  minutes = seconds == 59 && milliseconds == 1000 ? (minutes + 1) % 60 : minutes;
-  hours = minutes == 59 && seconds == 59 && milliseconds == 0 ? hours + 1 : hours;
+  seconds = milliseconds == 1000 ? (seconds + 1) % 60 : seconds;
+  minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1) % 60 : minutes;
+  hours = minutes == 0 && seconds == 0 && milliseconds == 0 ? hours + 1 : hours;
   milliseconds = milliseconds == 1000 ? 0 : milliseconds;
 
   let h = String(hours).padStart(2, "0");
@@ -23,3 +23,13 @@ function displayTimer() {
 
   timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
 }
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+  clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+  clearInterval(int);
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00 : 00 : 000";
+});
